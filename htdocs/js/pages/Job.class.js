b1c209e257f46f2700594f873f78606f13c98faa
Joseph Huckaby <jhuckaby@pixlcore.com>
2025-12-24 19:49:07 -0800

    Move confettiParty to base, so other pages can use it

--- htdocs/js/pages/Job.class.js
+++ htdocs/js/pages/Job.class.js
@@ -866,45 +866,6 @@ Page.Job = class Job extends Page.PageUtils {
 		} ); // api.get
 	}
 	
-	confettiParty() {
-		// yay!
-		var count = 200;
-		var defaults = {
-			origin: { y: 0.7 }
-		};
-		
-		function fire(particleRatio, opts) {
-			app.confetti({
-				...defaults,
-				...opts,
-				particleCount: Math.floor(count * particleRatio)
-			});
-		};
-		
-		fire(0.25, {
-			spread: 26,
-			startVelocity: 55,
-		});
-		fire(0.2, {
-			spread: 60,
-		});
-		fire(0.35, {
-			spread: 100,
-			decay: 0.91,
-			scalar: 0.8
-		});
-		fire(0.1, {
-			spread: 120,
-			startVelocity: 25,
-			decay: 0.92,
-			scalar: 1.2
-		});
-		fire(0.1, {
-			spread: 120,
-			startVelocity: 45,
-		});
-	}
-	
 	renderEventParams() {
 		// render event parameters (user fields)
 		var self = this;
